22a0c79b5fd2d474e70a8108bb5b56318c8c5a2c
yermaka <ermak.080@mail.ru>
2025-07-08 02:18:18 +0800

    store implements

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,1 +1,4 @@
-export {useAccountsStore} from ""
+export { useAccountsStore } from '~/shared/store/store'
+import BaseBtn from '~/shared/ui/BaseBtn.vue'
+import type { Account } from '~/shared/store/store'
+export { BaseBtn, type Account }
--- src/shared/store/store.ts
+++ src/shared/store/store.ts
@@ -1,6 +1,57 @@
 import { defineStore } from 'pinia'
 
+export interface Account {
+    id: number
+    mark: string | { text: string }[]
+    typeOfRecord: string
+    login: string
+    password: string | null
+    isValid: 'validate' | 'invalid' | 'valid'
+}
+
+interface AccountsStore {
+    countOfIDs: number
+    accounts: Account[]
+}
 
 export const useAccountsStore = defineStore('accounts', {
-
+    state: (): AccountsStore => ({
+        countOfIDs: 0,
+        accounts: [],
+    }),
+    actions: {
+        pushAccount(account: Account) {
+            this.accounts.push(account)
+        },
+        updateCount() {
+            this.countOfIDs = this.countOfIDs + 1
+        },
+        deleteById(id: number) {
+            this.accounts = this.accounts.filter((acc) => acc.id !== id)
+        },
+        getAccById(id: number) {
+            return this.accounts.find((acc) => acc.id === id)
+        },
+        acceptAccount(account: Account) {
+            const index = this.accounts.findIndex(
+                (acc) => acc.id === account.id,
+            )
+            if (index !== -1) {
+                account.isValid = 'valid'
+                const accounts = this.accounts
+                accounts[index] = { ...account }
+                this.accounts = accounts
+            }
+        },
+        updateAccount(account: Account) {
+            const index = this.accounts.findIndex(
+                (acc) => acc.id === account.id,
+            )
+            if (index !== -1) {
+                const accounts = this.accounts
+                accounts[index] = { ...account }
+                this.accounts = accounts
+            }
+        },
+    },
 })
