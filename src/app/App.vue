17c34d214ec700221e327d3890c46e73006d2287
yermaka <ermak.080@mail.ru>
2025-07-08 02:19:10 +0800

    add DefaultPage

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-
+    import DefaultPage from '~/pages/default.page.vue'
 </script>
 
 <template>
+    <DefaultPage />
 </template>
---
+++ src/pages/default.page.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    import { Header } from '~/entities'
+    import { AccountTable } from '~/entities'
+    import { Tip } from '~/entities'
+</script>
+
+<template>
+    <Header class="mb-8" />
+    <Tip class="mb-8 pl-20" />
+    <AccountTable class="mb-8" />
+</template>
