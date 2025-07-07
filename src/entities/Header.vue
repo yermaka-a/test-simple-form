03b0fa87849e99a906b795ea7e56886a58d35e78
yermaka <ermak.080@mail.ru>
2025-07-07 16:42:49 +0800

    create BaseBtn and Header

---
+++ src/entities/Header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { BaseBtn } from '~/shared'
+</script>
+
+<template>
+    <header class="bg-red-500 flex justify-center shadow-md shadow-red-400">
+        <div class="w-10/12 flex justify-start items-center p-[1rem]">
+            <h1 class="font-bold mr-3.5 text-4xl text-gray-200">
+                Учетные записи
+            </h1>
+            <BaseBtn />
+        </div>
+    </header>
+</template>
+
+<style scoped></style>
---
+++ src/shared/ui/BaseBtn.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+    <button
+        class="hover:shadow-md hover:shadow-amber-100 transition-transform hover:translate-y-[-3px] cursor-pointer border-2 p-4 rounded-full text-6xl text-gray-200 size-20 flex justify-center items-center bg-amber-200">
+        <span class="border-[3px] text-gray-100 w-10 rounded-3xl"></span>
+        <span
+            class="border-[3px] text-gray-100 w-10 rounded-3xl rotate-90 absolute"></span>
+    </button>
+</template>
+
+<style scoped></style>
