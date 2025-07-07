990c11977c6eb942e3b09cc5b85a921bb8fc40c4
yermaka <ermak.080@mail.ru>
2025-07-08 02:18:07 +0800

    AccountTable completed

--- src/entities/AccountTable/AccountTable.vue
+++ src/entities/AccountTable/AccountTable.vue
@@ -2,9 +2,60 @@
     import BaseInput from './ui/BaseInput.vue'
     import DeleteBtn from './ui/DeleteBtn.vue'
     import BaseSelect from './ui/BaseSelect.vue'
-    import { useAccountsStore } from '~/shared'
+    import { useAccountsStore, type Account } from '~/shared'
+    import MarkInput from './ui/MarkInput.vue'
 
     const store = useAccountsStore()
+
+    const onOut = (acc: Account) => {
+        if (acc.typeOfRecord === '1') {
+            acc.password = null
+        } else {
+            if (acc.password?.length) {
+                if (!(acc.password.length > 0 && acc.password?.length <= 100)) {
+                    acc.isValid = 'invalid'
+                    return
+                }
+            } else {
+                acc.isValid = 'invalid'
+                return
+            }
+        }
+        if (acc.login.length > 0 && acc.login.length <= 100) {
+            // валидация метки
+            console.log('mark', acc.mark)
+            if (
+                typeof acc.mark === 'string' &&
+                acc.mark.includes(';', 0) &&
+                acc.mark.length <= 50
+            ) {
+                const lastIndex = acc.mark.lastIndexOf(';')
+                const sub = acc.mark.slice(0, lastIndex)
+                const parts = sub.split(';')
+                acc.mark = parts.map((el) => ({
+                    text: el,
+                }))
+            }
+        } else {
+            acc.isValid = 'invalid'
+            return
+        }
+        store.acceptAccount(acc)
+    }
+
+    const updateModelVal = (payload: Array<{ text: string }>, acc: Account) => {
+        console.log(payload, acc)
+        if (payload !== null) {
+            if (acc?.mark !== undefined) {
+                acc.mark = payload
+                console.log('updateModelVal', acc.mark)
+                store.updateAccount(acc)
+            }
+        }
+    }
+    const deleteFieldById = (accountId: number) => {
+        store.deleteById(accountId)
+    }
 </script>
 <template>
     <main class="flex items-center justify-center">
@@ -18,27 +69,41 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="acc of store.accounts" :key="acc.id">
+                <tr
+                    @focusout="onOut(acc)"
+                    @change="onOut(acc)"
+                    v-for="acc of store.accounts"
+                    :key="acc.id">
                     <td>
-                        <BaseInput
-                            v-model="acc.mark"
+                        <MarkInput
+                            :model-value="acc.mark"
+                            @update-model-value="(payload: Array<{text: string}>)=>updateModelVal(payload, acc)"
                             type="text"
                             placeholder="Ваша метка..."
-                            :max-length="50" />
+                            :max-length="50"
+                            :disabled="
+                                acc.isValid === 'valid' ? true : false
+                            " />
                     </td>
                     <td>
-                        <BaseSelect v-model="acc.typeOfRecord" />
+                        <BaseSelect
+                            v-model="acc.typeOfRecord"
+                            :disabled="
+                                acc.isValid === 'valid' ? true : false
+                            " />
                     </td>
-                    <td>
+                    <td :colspan="acc.typeOfRecord !== '1' ? '1' : '2'">
                         <BaseInput
+                            :isValid="acc.isValid"
                             v-model="acc.login"
                             type="text"
                             required
                             :max-length="100"
                             placeholder="Введите логин..." />
                     </td>
-                    <td>
+                    <td v-if="acc.typeOfRecord === '2'">
                         <BaseInput
+                            :isValid="acc.isValid"
                             v-model="acc.password"
                             type="password"
                             required
@@ -46,7 +111,9 @@
                             placeholder="Введите пароль..." />
                     </td>
                     <td>
-                        <DeleteBtn :accountId="acc.id" />
+                        <DeleteBtn
+                            :accountId="acc.id"
+                            @click="deleteFieldById" />
                     </td>
                 </tr>
             </tbody>
