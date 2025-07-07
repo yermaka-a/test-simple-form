2ed0094b6b0464f6ce815bb317a0affb5d321d06
yermaka <ermak.080@mail.ru>
2025-07-07 20:00:06 +0800

    add Tip

---
+++ src/entities/Tip.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+    <section class="flex items-center justify-start">
+        <div
+            class="w-[50%] text-xl grid-cols-2 bg-gray-200 p-3 rounded-md flex items-center min-w-min">
+            <div
+                class="relative border-[1px] p-[30px] flex items-center mr-4 justify-center size-10 rounded-full">
+                <span
+                    class="text-2xl size-5 text-center align-text-top absolute top-[25%]"
+                    >?</span
+                >
+            </div>
+            <p class="inline-block">
+                Для указания нескольких меток для одной пары логин/пароль
+                используйте разделитель ;
+            </p>
+        </div>
+    </section>
+</template>
